c45c0ee57b725417f6291b114e79aaede7b481ca
Marek Skiba <skibamarek@gmail.com>
2016-09-07 00:10:06 +0200

    added support for submenu in menu

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -16,11 +16,13 @@ var nav = (function() {
         self.$window = $(window);
         self.$menu = $('.header__menu');
         self.$menuItems = $('.header__menu__item a');
+        self.$menuItemHasSubmenu = $('.header__menu__item--has-submenu');
+        self.$menuSubmenu = $('.header__menu__submenu');
         self.$hamburger = $('.header__hamburger');
         self.$hash = '';
-        self.$hamburgerOpen = false;
+        self.$mobileMenuOpen = false;
         self.$mobileMenuWidth = '240px';
-        self.sections = $('section').not('section section');
+        self.$sections = $('section').not('section section');
     };
 
     var load = function() {
@@ -32,7 +34,7 @@ var nav = (function() {
 
         self.$hash = window.location.hash;
         if (self.$hash) {
-            $('a[href="' + self.$hash + '"]').click();
+            self.$menuItems.mPageScroll2id('scrollTo', self.$hash);
         }
     };
 
@@ -43,55 +45,70 @@ var nav = (function() {
         self.$window.bind('scroll', scrollUrl);
         // show/close menu when clicking hamburger on mobile
         self.$hamburger.bind('click', clickHambuger);
+        // click/hover on menu with submenu
+        self.$menuItemHasSubmenu.bind('click mouseenter', showSubmenu);
+        self.$menuItemHasSubmenu.bind('mouseleave', hideSubmenu);
     };
 
-    // Clicks
-    var clickMenuItem = function(event) {
-        closeHambuger();
-        $('.header__menu--open .header__menu__item a').unbind('click');
+    // Submenu
+    var showSubmenu = function(event) {
+        if (self.$menuSubmenu.hasClass('header__menu__submenu--show')) {
+            return hideSubmenu();
+        }
+
+        self.$menuSubmenu.addClass('header__menu__submenu--show');
+        self.$menuItemHasSubmenu.addClass('header__menu__item--has-open-submenu');
+    };
+
+    var hideSubmenu = function(event) {
+        self.$menuSubmenu.removeClass('header__menu__submenu--show');
+        self.$menuItemHasSubmenu.removeClass('header__menu__item--has-open-submenu');
     };
 
     // Hamburger
     var clickHambuger = function(event) {
-        if (self.$hamburgerOpen === false) {
-            showHambuger();
+        if (self.$mobileMenuOpen === false) {
+            showMobileMenu();
         } else {
-            closeHambuger();
+            closeMobileMenu();
         }
     };
 
-    var showHambuger = function() {
-        self.$hamburgerOpen = true;
+    // Mobile menu
+    var showMobileMenu = function() {
+        self.$mobileMenuOpen = true;
 
         self.$hamburger.addClass('header__hamburger--open');
         self.$menu.addClass('header__menu--open');
         self.$menu.animate({ left: '0' }, 600);
         self.$hamburger.animate({ left: self.$mobileMenuWidth }, 0);
-        self.sections.css('opacity', '0.4');
-        self.sections.animate({ left: self.$mobileMenuWidth }, 600);
+        self.$sections.addClass('opacity');
+        self.$sections.animate({ left: self.$mobileMenuWidth }, 600);
         // close menu when user clicked some position of menu
-        $('.header__menu--open .header__menu__item a').bind('click', clickMenuItem);
+        $('.header__menu--open .header__menu__item a').bind('click', closeMobileMenu);
     };
 
-    var closeHambuger = function() {
-        self.$hamburgerOpen = false;
+    var closeMobileMenu = function() {
+        self.$mobileMenuOpen = false;
 
         self.$menu.animate({ left: '-' + self.$mobileMenuWidth }, 600);
         self.$hamburger.animate({ left: '15px' }, 0);
-        self.sections.animate({ left: '0' }, 600, function() {
+        self.$sections.animate({ left: '0' }, 600, function() {
             self.$menu.removeClass('header__menu--open');
-            self.sections.css('opacity', '1');
+            self.$sections.removeClass('opacity');
         });
         self.$hamburger.removeClass('header__hamburger--open');
+        $('.header__menu--open .header__menu__item a').unbind('click');
     };
-
 
     // Scrolls
     var scrollMenu = function(event) {
-        if (self.$window.scrollTop() > 40) {
-            self.$menu.addClass('header__menu--scroll');
+        var origOffsetY = document.querySelector('.header__menu__item').offsetTop;
+
+        if (self.$window.scrollTop() > origOffsetY) {
+            self.$menu.addClass('header__menu--sticky');
         } else {
-            self.$menu.removeClass('header__menu--scroll');
+            self.$menu.removeClass('header__menu--sticky');
         }
     };
 
